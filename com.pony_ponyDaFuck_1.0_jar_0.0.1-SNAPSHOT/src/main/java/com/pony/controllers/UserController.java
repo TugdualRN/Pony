0b544b00388588b5c8b7262d231479828103133d
Fynzie <fynz@hotmail.fr>
2018-06-16 20:16:06 +0200

    removed useless stuff

--- com.pony_ponyDaFuck_1.0_jar_0.0.1-SNAPSHOT/src/main/java/com/pony/controllers/UserController.java
+++ com.pony_ponyDaFuck_1.0_jar_0.0.1-SNAPSHOT/src/main/java/com/pony/controllers/UserController.java
@@ -1,12 +1,9 @@
 package com.pony.controllers;
 
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
