2136a3d605ffe4206580f60a8dcb341ec2813d31
FynZ <fynz@hotmail.fr>
2018-07-17 16:03:06 +0200

    added db for tests

---
+++ com.pony_ponyDaFuck_1.0_jar_0.0.1-SNAPSHOT/src/main/java/com/pony/utils/RegisterResult.java
@@ -0,0 +1,12 @@
+// package com.utils;
+
+// public class RegisterResult {
+    
+//     private boolean isUserNameAlreadyTaken = false;
+
+//     private boolean isMailAlreadyTaken = false;
+    
+//     public RegisterResult
+//     {
+//     }
+// }
