12bbb4a28735b8caf5f4fb05f604f85796ea9ec4
guingrk <guingrich.kevin@gmail.com>
2018-11-30 09:45:46 +0100

    feature news

--- com.pony_ponyDaFuck_1.0_jar_0.0.1-SNAPSHOT/src/main/java/com/pony/views/viewmodels/NewsViewModel.java
+++ com.pony_ponyDaFuck_1.0_jar_0.0.1-SNAPSHOT/src/main/java/com/pony/views/viewmodels/NewsViewModel.java
@@ -4,20 +4,19 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class NewsViewModel {
-	
-    @NotNull
-    @Size(min = 3, max = 100)
-    private String title;
-    
-    @NotNull
-    @Size(min = 10)
-    private String content;
 
-    @NotNull
-    @Size(min = 3, max = 250)
-    private String description;
-   
-    
+	@NotNull
+	@Size(min = 3, max = 100)
+	private String title;
+
+	@NotNull
+	// @Size(min = 10)
+	private String content;
+
+	@NotNull
+	@Size(min = 3, max = 250)
+	private String description;
+
 	public String getTitle() {
 		return title;
 	}
